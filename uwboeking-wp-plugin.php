9f2b2ee3cacc2abe11fc26c61b0888c1b1e2686a
geert <geert@webgeert.nu>
2021-12-22 14:17:47 +0100

    v1.1.5

--- uwboeking-wp-plugin.php
+++ uwboeking-wp-plugin.php
@@ -16,7 +16,7 @@
  * Plugin Name:       UwBoeking-plugin
  * Plugin URI:        brandom.nl
  * Description:       UwBoeking-plugin voor WordPress
- * Version:           1.1.0
+ * Version:           1.1.5
  * Author:            Geert van Dijk
  * Author URI:        brandom.nl
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'UWBOEKING_WP_PLUGIN_VERSION', '1.0.0' );
+define( 'UWBOEKING_WP_PLUGIN_VERSION', '1.1.5' );
 
 /**
  * The code that runs during plugin activation.
